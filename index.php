d8960a35bd17563e3455256d9c1dbfc4f5356821
Mentor Elshani <mentorelshani04@gmail.com>
2018-05-26 18:03:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href=" " integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<style type="text/css"></style>
+</head>
+<body>
+	<div class="content">
+		<?php include('header.html'); ?>
+		<div class="main">
+			<div class="articles">
+				<div class="article-large">
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+					</span>
+					<img src="foto\people-magazine.svg" class="img">
+				</div>
+				<div class="mediums">
+					<div class="article-medium">
+						<span>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+						</span>
+						<hr>
+						<img src="foto\people-magazine.svg">
+						<hr>
+						<div class="medium-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						<a href="">continue reading</a></div>
+					</div>
+					<div class="article-medium">
+						<span>
+							Duis aute irure dolor in reprehenderit in voluptate velit esse						cillum dolore 
+						</span>
+						<hr>
+						<img src="foto\19858310_BG2.webp">
+						<hr>
+						<div class="medium-body">Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat nont esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat nonUt enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat nont esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat non
+						<a href="">continue reading</a></div>
+					</div>	
+					<div class="article-medium">
+						<span>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+						</span>
+						<hr>
+						<img src="foto\people-magazine.svg">
+						<hr>
+						<div class="medium-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						<a href="">continue reading</a></div>
+					</div>
+					<div class="article-medium">
+						<span>
+							Duis aute irure dolor in reprehenderit in voluptate velit esse						cillum dolore 
+						</span>
+						<hr>
+						<img src="foto\19858310_BG2.webp">
+						<hr>
+						<div class="medium-body">Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat nont esse
+						cillum dolore eu fugiat nullon ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						continue reading pariatur. Excepteur sint occaecat cupidatat non
+						<a href="">continue reading</a></div>
+					</div>				
+				</div>
+			</div>
+			<div class="news">
+				<div class="search-form">
+					<input t-ype="" name="" placeholder="Enter search" class="input">
+					<button class="btn">Search</button>
+				</div>
+				<div class="social-media">
+					<div class="media">
+						<img src="foto/instagram.png" class="media-img">
+						<div class="media-text">5214 followers</div>
+					</div>
+					<div class="media">
+						<img src="foto/instagram.png" class="media-img">
+						<div class="media-text">5214 followers</div>
+					</div>
+				</div>
+				<br>
+				<div class="top-feed nav nav-tabs">
+					<li class="selected">Popular</li>
+					<li>Latest</li>
+					<li>Comments</li>
+					<div class="art-small-two">
+						<hr>
+						<div class="article-small">
+							<span class="title">Lorem ipsum dolor sit amet.</span>
+							<br><br>
+							<img src="foto\people-magazine.svg">
+							<div>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+							Lorem ipsum d<a href="">asd</a>
+							</div>
+						</div>
+						<hr>
+						<div class="article-small">
+							<span class="title">Lorem ipsum dolor sit amet.</span>
+							<br><br>
+							<img src="foto\people-magazine.svg">
+							<div>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+							Lorem ipsum d<a href="">asd</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div>
+						<div class="menu-2">
+							<div class="title">Categories</div>
+							<br>
+							<div class="options">
+								<a href="">Design</a>
+								<a href="">Inspiration</a>
+								<a href="">Wordpress</a>
+								<a href="">Photography</a>
+							</div>
+						</div>
+						<div class="menu-2" style="float: right;">
+							<div class="title">Site pages</div>
+							<br>
+							<div class="options">
+								<a href="">Home</a>
+								<a href="">About</a>
+								<a href="">Archive</a>
+								<a href="">Contact us</a>
+							</div>
+						</div>
+				</div>
+				<div class="video">
+					<div><span>Latest videos</span></div>
+					<iframe class="video-player" src="https://www.youtube.com/embed/LQ488QrqGE4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript">
+
+	var articles = document.getElementsByClassName('medium-body');
+	console.log(articles);
+	for (var i = 0; i < articles.length; i++) {
+		if(articles[i].innerHTML.length > 300)
+			articles[i].innerHTML = articles[i].innerHTML.substring(0,400) + " [...]";
+	}
+
+	var a =document.getElementById('qas').innerHTML;
+	console.log(a);
+	console.log(a.substring(0,50));
+
+	document.getElementById('qas').innerHTML = a.substring(0,300);
+
+
+	console.log(document.getElementById('qas').offsetHeight);
+	console.log(document.getElementById('qas').scrollHeight);
+</script>
+</html>
