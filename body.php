07aab021397b24a2404aa979b02330a87e5120a5
Triumf <triumf_96@hotmail.com>
2018-06-02 19:43:50 +0200

    Aside with out Filter articles

--- body.php
+++ body.php
@@ -49,7 +49,7 @@
 				<span class="article__content--more">Contunue reading</span>
 			</div>
 		</div>
-		
+
 		<div class="top__article--right">
 			<div class="article__body">
 				<span class="article__title">25 Awesome posters about graphic design</span>
@@ -68,20 +68,20 @@
 
 	<div class="aside">
 		<div class="search">
-			<input type="text" name="" placeholder="Exten Search ...">
-			<button type="button"> Search</button>
+			<input type="text" name="" placeholder="Exten Search ..." class="search__input">
+			<button type="button" class="search__button"> Search</button>
 		</div>
 
 		<div class="followers">
-			<div class="followers__rss followers__item">
-				<img src="" alt="" class="followers__item--icon">
-				<span class="followers__item--number"></span>
-				<span class="followers__item--description"></span>
+			<div class="followers__item">
+				<img src="./img/RSS_f.png" alt="" class="followers__item--icon">
+				<span class="followers__item--number">12550</span>
+				<span class="followers__item--description">Subscribers</span>
 			</div>
-			<div class="followers__rss followers__item">
-				<img src="" alt="" class="followers__item--icon">
-				<span class="followers__item--number"></span>
-				<span class="followers__item--description"></span>
+			<div class="followers__item">
+				<img src="./img/Icon_1.png" alt="" class="followers__item--icon">
+				<span class="followers__item--number">7134</span>
+				<span class="followers__item--description">Followers</span>
 			</div>
 		</div>
 
@@ -102,24 +102,26 @@
 		</div>
 
 		<div class="categories">
-			<span class="categories__name"></span>
-			<span class="categories__item"></span>
-			<span class="categories__item"></span>
-			<span class="categories__item"></span>
-			<span class="categories__item"></span>
+			<span class="categories__name">Categories</span>
+			<span class="categories__item">Design</span>
+			<span class="categories__item">Inspiration</span>
+			<span class="categories__item">Wordpress</span>
+			<span class="categories__item">Photography</span>
 		</div>
 
 		<div class="sitePages">
-			<span class="sitePages__name"></span>
-			<span class="sitePages__item"></span>
-			<span class="sitePages__item"></span>
-			<span class="sitePages__item"></span>
-			<span class="sitePages__item"></span>
+			<span class="sitePages__name">Site Pages</span>
+			<span class="sitePages__item">Home</span>
+			<span class="sitePages__item">About</span>
+			<span class="sitePages__item">Acchives</span>
+			<span class="sitePages__item">Contact us</span>
 		</div>
 
 		<div class="last__video">
-			<span class="discription"></span>
-			<div class="video"></div>
+			<span class="discription">Laters videos</span>
+			<div class="video">
+				<div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/ApUl3Ops69M?ecver=2" style="position:absolute;width:100%;height:100%;left:0" width="640" height="360" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+			</div>
 		</div>
 	</div>
 </div>
